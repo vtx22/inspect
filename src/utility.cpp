c76dbafcedbf76a26971b114570dd639e6a967ae
Fynn Raddatz <fynn.raddatz@gmail.com>
2024-05-05 23:42:11 +0200

    open file with file explorer

--- src/utility.cpp
+++ src/utility.cpp
@@ -6,3 +6,23 @@ ImTextureID gl_handle_to_imgui_id(GLuint gl_texture_handle)
     std::memcpy(&textureID, &gl_texture_handle, sizeof(GLuint));
     return textureID;
 }
+
+std::string open_file_explorer(HWND hwnd)
+{
+    std::string path(MAX_PATH, '\0');
+    OPENFILENAME ofn = {sizeof(OPENFILENAME)};
+    ofn.hwndOwner = hwnd;
+    ofn.lpstrFilter = "Image files (*.jpg;*.png;*.bmp)\0*.jpg;*.png;*.bmp\0";
+    //"All files\0*.*\0";
+    ofn.lpstrFile = &path[0];
+    ofn.nMaxFile = MAX_PATH;
+    ofn.Flags = OFN_FILEMUSTEXIST;
+    if (GetOpenFileName(&ofn))
+    {
+        // string::size() is still MAX_PATH
+        // strlen is the actual string size (not including the null-terminator)
+        // update size:
+        path.resize(strlen(path.c_str()));
+    }
+    return path;
+}
--- src/utility.hpp
+++ src/utility.hpp
@@ -6,7 +6,11 @@
 
 #include <cstring>
 #include <cmath>
+#include <string>
+#include <windows.h>
 
 ImTextureID gl_handle_to_imgui_id(GLuint gl_texture_handle);
+
+std::string open_file_explorer(HWND hwnd);
 
 #endif // INSPECT_HPP
